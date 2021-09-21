02d213b92279444a012663d6d8fef848d095369a
Bindusrihp <shreebindu7760388893@gmail.com>
2021-09-21 20:14:03 +0530

    writes DataTypeTest program

---
+++ DataTypeTest.java
@@ -0,0 +1,27 @@
+class DataTypeTest{
+
+public static void main(String[] args){	s
+
+short s=30000;
+System.out.println("s:"+s);
+
+int i=100000;//till 10 digit number it can hold
+System.out.println("i:"+i);
+
+long l=10000000000l;
+System.out.println("l:"+l);
+
+float f=10.00f;
+System.out.println("f:"+f);
+
+double d=123.45;
+System.out.println("d:"+d);
+
+char c='b';
+System.out.println("c:"+c);
+
+boolean bo=true;
+System.out.println("bo:"+bo);
+
+}
+}
