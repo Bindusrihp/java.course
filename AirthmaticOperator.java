c74848441a0d9a7020def57b81f85cb5c187be8f
Bindusrihp <shreebindu7760388893@gmail.com>
2021-09-22 10:25:47 +0530

    writes AirthmaticOperator program

---
+++ AirthmaticOperator.java
@@ -0,0 +1,31 @@
+class AirthmaticOperator{
+public static void main(String[] args)
+{
+int a=14;
+int b=7;
+int addResult=a+b;
+System.out.println("addResult:"+addResult);
+
+int c=14;
+int d=7;
+int subResult=c-d;
+System.out.println("subResult:"+subResult);
+
+int e=14;
+int f=7;
+int mulResult=e*d;
+System.out.println("mulResult:"+mulResult);
+
+int g=14;
+int h=7;
+int divResult=g/h;
+System.out.println("divResult:"+divResult);
+
+int i=14;
+int j=7;
+int modResult=i%j;
+System.out.println("modResult:"+modResult);
+
+
+}
+}
