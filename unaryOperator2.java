ef4a36fcc1387658c91d357fd1adb5dce7ab5d8d
Bindusrihp <shreebindu7760388893@gmail.com>
2021-09-22 10:49:22 +0530

    writes unaryOperator2 program

---
+++ unaryOperator2.java
@@ -0,0 +1,17 @@
+class unaryOperator2{
+public static void main(String[] args)
+{
+System.out.println("pre increment operation:");
+int a=99;
+int b=++a;
+System.out.println("a:"+a );
+System.out.println("b:"+b);
+
+
+System.out.println("post increment operation:");
+int c=99;
+int d=c++;
+System.out.println("c:"+c );
+System.out.println("d:"+d);
+}
+}
