ccdd0465d886fcd6bd68de69c69eb34d1eaa8684
Bindusrihp <shreebindu7760388893@gmail.com>
2021-09-22 10:46:15 +0530

    writes negationOperator2 program

---
+++ negationOperator2.java
@@ -0,0 +1,10 @@
+class negationOperator2{
+public static void main(String[] args)
+{
+System.out.println("negation operation:");
+boolean z=false;
+boolean result=!z;
+System.out.println("z:"+z );
+System.out.println("result:"+result);
+}
+}
