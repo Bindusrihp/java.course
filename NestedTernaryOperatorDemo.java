9c5259aff8dab85a11eb1fbcc5edd1de256e5321
Bindusrihp <shreebindu7760388893@gmail.com>
2021-09-27 21:06:54 +0530

    writes NestedTernaryOperatorDemo program

---
+++ NestedTernaryOperatorDemo.java
@@ -0,0 +1,9 @@
+class NestedTernaryOperatorDemo{
+public static void main(String[] args)
+{
+int x=50,y=100,z=150;
+int largest=(x>y)?((x>y)?x:z):((y>z)?y:z);
+System.out.println("Largest Number:"+largest);
+}
+}
+
