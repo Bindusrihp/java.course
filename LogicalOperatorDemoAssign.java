ab1cb46faf625017fba7d9a3d597d9a7b1c061f2
Bindusrihp <shreebindu7760388893@gmail.com>
2021-09-27 20:58:52 +0530

    writes LogicalOperatorDemoAssign program

---
+++ LogicalOperatorDemoAssign.java
@@ -0,0 +1,17 @@
+class LogicalOperatorDemoAssign{
+public static void main(String[] args)
+{
+int a = 10 ;
+int b = 20 ;
+System.out.println( "Logical AND operator");
+System.out.println( a>b && a++<b);
+
+System.out.println( "Logical OR operator");
+System.out.println( a>b || a++<b);
+
+System.out.println( "Logical NOT operator");
+System.out.println( !(a>b));
+
+}
+
+}
