b2511e86468717d5488e3ef2c2962ac31d0e718e
Bindusrihp <shreebindu7760388893@gmail.com>
2021-10-01 18:10:40 +0530

    writes TwoDigit2 program

---
+++ TwoDigit2.java
@@ -0,0 +1,16 @@
+class TwoDigit2
+{
+public static void main(String args[])
+{
+ int num = 97987499 ;
+
+if(num >= 10 && num < 100)
+{
+System.out.println("it is two digit number");
+}
+else
+{
+System.out.println("it is Not Two-Digit Number");
+}
+}
+}
