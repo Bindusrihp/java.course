e3194ea3c843fcfb1a431f448fca6c99a6db1399
Bindusrihp <shreebindu7760388893@gmail.com>
2021-10-01 18:12:27 +0530

    writes MultipleIf1 program

---
+++ MultipleIf1.java
@@ -0,0 +1,32 @@
+class MultipleIf1{
+      public static void main(String args[]){
+	int num=7;
+	if(num == 1) {
+	  System.out.println("Monday");
+	}
+	if(num == 2) {
+	  System.out.println("Tuesday");
+	}
+if(num == 3) {
+	  System.out.println("Wednesday");
+	}
+if(num == 4) {
+	  System.out.println("Thursday");
+	}
+if(num == 5) {
+	  System.out.println("Friday");
+	}
+if(num == 6) {
+	  System.out.println("Saturday");
+	}
+if(num == 7) 
+{
+	  System.out.println("Sunday");
+	}
+if(num > 7) 
+{
+	  System.out.println("Plz enter number between 1 to 7");
+	}
+
+      }
+    }
