0660bca867042b013a6907addfc6ddc7d72f0aec
Bindusrihp <shreebindu7760388893@gmail.com>
2021-10-01 18:23:38 +0530

    writes HelloDevelopersJokers1 program

---
+++ HelloDevelopersJokers1.java
@@ -0,0 +1,15 @@
+public class HelloDevelopersJokers1
+{
+public static void main(String[] args) 
+{
+boolean helloDevelopers=false;
+if(helloDevelopers == true){
+
+System.out.println("helloDevelopers"); 
+}
+ else
+{
+System.out.println("helloJokers");
+}
+}
+}
